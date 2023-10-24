2dac33a5129d9640cd7d25b472d74bdf82f44846
Rolo <roloedits@gmail.com>
2023-10-23 22:23:03 -0700

    refactor: make types to hold state

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,209 @@
 // PID,NAME,CPU,MEM,VIRT_MEM
 
 use anyhow::Context;
+use anyhow::Result;
 use clap::{Parser, ValueEnum};
+use std::fmt::Display;
 use std::fs::File;
 use std::io::Write;
+use std::process::ExitStatus;
 use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
 use std::{path::PathBuf, process::Command};
-use sysinfo::{CpuExt, Pid, ProcessExt, System, SystemExt};
+use sysinfo::{ProcessExt, System, SystemExt};
 use uuid::Uuid;
 
-// TODO: Need to figure out the message passing to control when to break out of loop and write data out.
-// Need to handle when there is an error with the running program if spawned from the positional argument.
-// NOTE: When the interput is sent, if the program was running from the positional argument, when it should be killed too.
-
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    let (program_tx, program_rx) = channel();
+    let mut peek = Peek::new(cli)?;
 
-    let mut peep_spawned = false;
+    peek.run()?;
 
-    let pid = cli.pid.unwrap_or_else(|| {
-        let mut process = Command::new(format!(
-            "./{}",
-            cli.program.expect("no program was passed in to run")
-        ))
-        .spawn()
-        .expect("Failed to run program");
-
-        peep_spawned = true;
-
-        let pid = process.id() as usize;
-
-        std::thread::spawn(move || {
-            let status = process.wait().expect("program failed to run");
-            program_tx
-                .send(status)
-                .expect("failed to send finsihed programs status back to peep");
-        });
-
-        pid
-    });
-
-    let (ctl_c_tx, ctrl_c_rx) = channel();
-
-    ctrlc::set_handler(move || {
-        ctl_c_tx
-            .send(())
-            .expect("failed to send interupt signal through channel")
-    })
-    .expect("error setting Ctrl-C handler");
-
-    let mut data: Vec<Data> = Vec::with_capacity(1024);
-    let uuid = Uuid::new_v4();
-    let mut sample = 0;
-
-    let mut sys = System::new_all();
-    let threads = sys.cpus().len();
-    // let cores = sys.physical_core_count().expect("failed to get core count");
-
-    loop {
-        if program_rx.try_recv().is_ok() {
-            // program finished running
-            break;
-        }
-
-        sys.refresh_all();
-
-        let process = sys
-            .process(Pid::from(pid))
-            .with_context(|| "no such proces is running")?;
-
-        // Was manually interupted
-        if ctrl_c_rx.try_recv().is_ok() {
-            // If was manually interupted and the process was started from peep
-            // then kill the process
-            if peep_spawned {
-                process.kill();
-            }
-            break;
-        }
-
-        let name = process.name();
-        let cpu = process.cpu_usage() / threads as f32;
-        let mem = process.memory();
-        let virt_mem = process.virtual_memory();
-        let cpu_freq = sys
-            .cpus()
-            .iter()
-            .fold(0, |freq, cpu| cpu.frequency() + freq)
-            / threads as u64;
-        let disk_read = process.disk_usage().total_read_bytes;
-        let disk_write = process.disk_usage().total_written_bytes;
-
-        data.push(Data {
-            uuid,
-            sample,
-            pid,
-            name: name.to_string(),
-            cpu,
-            cpu_freq,
-            mem,
-            virt_mem,
-            disk_read,
-            disk_write,
-        });
-
-        sample += 1;
-
-        // 200ms
-        std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
-    }
-
-    match cli.format {
-        Format::Csv => to_csv(data),
-        Format::Json => to_json(cli.output, data),
-    }
+    peek.output()?;
 
     Ok(())
 }
 
-fn to_csv(data: Vec<Data>) {
-    todo!()
+struct Peek {
+    system: System,
+    program: Program,
+    output_path: PathBuf,
+    format: Format,
+    output: Output,
+    samples: Vec<Samples>,
+    crtl_c_interupt: Receiver<()>,
 }
 
-fn to_json(path: PathBuf, data: Vec<Data>) {
-    let json = serde_json::json!(data);
+impl Peek {
+    fn new(cli: Cli) -> Result<Self> {
+        let (tx, rx) = channel();
+        ctrlc::set_handler(move || tx.send(()).unwrap())?;
 
-    let mut cwd = std::env::current_dir().expect("couldn't get cwd");
+        let program = Program::new(&cli)?;
 
-    cwd.push(path);
+        Ok(Self {
+            system: System::new(),
+            program,
+            output_path: cli.path.unwrap_or_else(|| {
+                let mut output = std::env::current_dir().expect("couldn't get cwd");
+                output.push(format!("peek.{}", cli.format));
+                output
+            }),
+            crtl_c_interupt: rx,
+            format: cli.format,
+            samples: Vec::with_capacity(1024),
+            output: cli.output,
+        })
+    }
 
-    let mut file = File::create(cwd).expect("failed to get file handle");
-    file.write_all(json.to_string().as_bytes())
-        .expect("failed to write data to file");
+    fn run(&mut self) -> Result<()> {
+        let uuid = Uuid::new_v4();
+
+        let program = self.program.run()?;
+
+        self.system.refresh_processes();
+        self.system.refresh_cpu();
+        let threads = self.system.cpus().len();
+
+        let mut sample = 0;
+
+        loop {
+            if program.finished_running.try_recv().is_ok()
+                || self.crtl_c_interupt.try_recv().is_ok()
+            {
+                break;
+            }
+
+            self.system.refresh_processes();
+
+            let process = self
+                .system
+                .process(sysinfo::Pid::from(program.pid))
+                .with_context(|| "no such process is running")?;
+
+            self.samples.push(Samples {
+                uuid,
+                sample,
+                pid: program.pid,
+                name: process.name().to_string(),
+                cpu: process.cpu_usage() / threads as f32,
+                mem: process.memory(),
+                virt_mem: process.virtual_memory(),
+                disk_read: process.disk_usage().total_read_bytes,
+                disk_write: process.disk_usage().total_written_bytes,
+            });
+
+            sample += 1;
+
+            // 200ms
+            std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
+        }
+        Ok(())
+    }
+
+    fn output(self) -> Result<()> {
+        fn to_json(data: Vec<Samples>) -> String {
+            serde_json::json!(data).to_string()
+        }
+
+        fn to_csv(_data: Vec<Samples>) -> String {
+            todo!()
+        }
+
+        let data = match self.format {
+            Format::Csv => to_csv(self.samples),
+            Format::Json => to_json(self.samples),
+        };
+
+        match self.output {
+            Output::File => {
+                let mut file = File::create(self.output_path)?;
+
+                file.write_all(data.as_bytes())?;
+            }
+            Output::Stdout => {
+                println!("{data}");
+            }
+        }
+
+        Ok(())
+    }
 }
 
+#[derive(Debug)]
+struct Program {
+    command: String,
+    args: Vec<String>,
+}
+
+impl Program {
+    pub fn new(cli: &Cli) -> Result<Self> {
+        let command: Vec<String> = cli
+            .program
+            .split_whitespace()
+            .map(|param| param.to_owned())
+            .collect();
+
+        Ok(Self {
+            command: command[0].clone(),
+            args: command[1..].to_vec(),
+        })
+    }
+
+    pub fn run(&self) -> Result<RunningProgram> {
+        let (status_tx, status_rx) = channel();
+        let (pid_tx, pid_rx) = channel();
+
+        let command = self.command.clone();
+        let args = self.args.clone();
+
+        std::thread::spawn(move || {
+            let command = command;
+            let args = args;
+
+            if let Ok(mut child) = Command::new(&command).args(&args).spawn() {
+                pid_tx.send(child.id()).unwrap();
+
+                status_tx
+                    .send(child.wait().unwrap())
+                    .expect("failed to send finsihed programs status back to peep");
+            } else {
+                let cwd = std::env::current_dir().unwrap();
+                let command = cwd.join(&command);
+
+                let mut child = Command::new(command).args(&args).spawn().unwrap();
+
+                pid_tx.send(child.id()).unwrap();
+
+                status_tx
+                    .send(child.wait().unwrap())
+                    .expect("failed to send finsihed programs status back to peep");
+            }
+        });
+
+        let pid = pid_rx.recv().unwrap() as usize;
+
+        Ok(RunningProgram {
+            pid,
+            finished_running: status_rx,
+        })
+    }
+}
+
+struct RunningProgram {
+    pid: usize,
+    finished_running: Receiver<ExitStatus>,
+}
+
+// TODO: Time the samples and store in field.
 #[derive(Debug, serde::Serialize)]
-struct Data {
+struct Samples {
     uuid: Uuid,
     sample: u64,
     pid: usize,
     name: String,
     cpu: f32,
-    cpu_freq: u64,
     mem: u64,
     virt_mem: u64,
     disk_read: u64,
@@ -153,15 +213,17 @@ struct Data {
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    program: Option<String>,
-    #[arg(long, short)]
-    pid: Option<usize>,
-    #[arg(long, short)]
-    output: PathBuf,
-    #[arg(value_enum, long, short)]
+    program: String,
+    // #[arg(long, short)]
+    // pid: Option<usize>,
+    path: Option<PathBuf>,
+    #[arg(long, short, default_value = "stdout")]
+    output: Output,
+    #[arg(value_enum, long, short, default_value = "json")]
     format: Format,
 }
 
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Output {
     File,
     Stdout,
@@ -171,4 +233,15 @@ enum Output {
 enum Format {
     Csv,
     Json,
+}
+
+impl Display for Format {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Self::Csv => "csv",
+            Self::Json => "json",
+        };
+
+        write!(f, "{}", str)
+    }
 }
